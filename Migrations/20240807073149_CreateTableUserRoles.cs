bdf2e8fb235c1e0e00ec6da4d84ca3055115af60
dhonstudio <doon13@gmail.com>
2024-08-07 14:35:05 +0700

    dotnet ef migrations add CreateTableUserRoles

---
+++ Migrations/20240807073149_CreateTableUserRoles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace traningday2.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTableUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                schema: "ripas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IDUser = table.Column<int>(type: "int", nullable: false),
+                    IDRole = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserRoles",
+                schema: "ripas");
+        }
+    }
+}
