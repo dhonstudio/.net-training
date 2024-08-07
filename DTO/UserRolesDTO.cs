9a37da03a12d0d16c281ad9ec9d5056c7e183882
dhonstudio <doon13@gmail.com>
2024-08-07 15:05:03 +0700

    update return addrole

--- DTO/UserRolesDTO.cs
+++ DTO/UserRolesDTO.cs
@@ -2,6 +2,17 @@
 {
     public class UserRolesDTO
     {
+        public string Username { get; set; }
         public int IDRole { get; set; }
+        public string RoleName { get
+            {
+                return IDRole switch
+                {
+                    1 => "Admin",
+                    2 => "Pengelola",
+                    _ => "User"
+                };
+            }
+        }
     }
 }
--- Models/UserRoles.cs
+++ Models/UserRoles.cs
@@ -1,9 +1,13 @@
-﻿namespace traningday2.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace traningday2.Models
 {
     public class UserRoles
     {
         public int ID { get; set; }
         public int IDUser { get; set; }
+        [NotMapped]
+        public string Username { get; set; }
         public int IDRole { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime? UpdatedAt { get; set; }
