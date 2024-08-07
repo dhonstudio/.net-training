059e9682c603d72023714af2901c10922576a5cf
dhonstudio <doon13@gmail.com>
2024-08-07 11:59:52 +0700

    dotnet ef migrations add CreateTableUsers

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using traningday2.DTO;
+using traningday2.Models;
+
+namespace traningday2.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly SchoolContext _schoolContext;
+        private readonly IMapper _mapper;
+
+        public UsersController(SchoolContext schoolContext, IMapper mapper)
+        {
+            _schoolContext = schoolContext;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public IActionResult PostUsers(UsersParamDTO usersParam)
+        {
+            var user = _mapper.Map<Users>(usersParam);
+            var userExist = _schoolContext.Users.Any(x => x.Username == user.Username);
+            if (userExist) return BadRequest(new
+            {
+                Message = "user sudah ada"
+            });
+
+            _schoolContext.Users.Add(user);
+
+            _schoolContext.SaveChanges();
+            return Ok(_mapper.Map<UsersDTO>(user));
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,13 @@
+﻿namespace traningday2.Models
+{
+    public class Users
+    {
+        public int ID { get; set; }
+        public string FirstMidName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
