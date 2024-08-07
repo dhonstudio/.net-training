d6630070195e34f16c177e43b56c9fe9c4ea55b7
dhonstudio <doon13@gmail.com>
2024-08-07 13:34:58 +0700

    login with user in db

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -7,17 +7,19 @@ namespace traningday2.Controllers
     public class AuthController : Controller
     {
         private readonly ITokenService _tokenService;
+        private readonly SchoolContext _schoolContext;
 
-        public AuthController(ITokenService tokenService)
+        public AuthController(ITokenService tokenService, SchoolContext schoolContext)
         {
             _tokenService = tokenService;
+            _schoolContext = schoolContext;
         }
 
         [HttpPost("login")]
         public IActionResult Login([FromBody] LoginModel model)
         {
             // Validasi user di sini
-            if (model.Username == "string" && model.Password == "string")
+            if (_schoolContext.Users.FirstOrDefault(x => x.Username == model.Username && x.Password == model.Password) != null)
             {
                 var token = _tokenService.GenerateToken(model.Username);
                 return Ok(new { Token = token });
